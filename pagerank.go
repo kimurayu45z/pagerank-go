2888fcbb2bab22129d77006bfe56e55df3150b62
unknown <33382781+YuKimura45z@users.noreply.github.com>
2019-09-29 16:10:30 +0900

    [update]matrix-get-set

--- pagerank.go
+++ pagerank.go
@@ -1,14 +1,47 @@
 package pagerank
 
+import (
+	"errors"
+)
+
 /*
 Vector Vector
 */
-type Vector = map[string]float64
+type Vector map[string]float64
 
 /*
 Matrix Matrix
 */
-type Matrix = map[string]Vector
+type Matrix map[string]Vector
+
+/*
+Get Get
+*/
+func (matrix Matrix) Get(src string, dst string) float64 {
+	_, ok := matrix[src]
+	if !ok {
+		return 0
+	}
+
+	return matrix[src][dst]
+}
+
+/*
+Set Set
+*/
+func (matrix Matrix) Set(src string, dst string, value float64) {
+	if value == 0 {
+		delete(matrix[src], dst)
+		if len(matrix[src]) == 0 {
+			delete(matrix, src)
+		}
+		return
+	}
+	if matrix[src] == nil {
+		matrix[src] = Vector{}
+	}
+	matrix[src][dst] = value
+}
 
 /*
 GetStochastixMatrix GetStochastixMatrix
@@ -16,16 +49,16 @@ GetStochastixMatrix GetStochastixMatrix
 func GetStochastixMatrix(linkMatrix Matrix) Matrix {
 	stochasticMatrix := Matrix{}
 
-	for src, vector := range linkMatrix {
+	for src := range linkMatrix {
 		sum := float64(0)
 
-		for _, value := range vector {
-			sum += value
+		for dst := range linkMatrix[src] {
+			sum += linkMatrix[src][dst]
 		}
 
 		v := Vector{}
 
-		for dst, value := range vector {
+		for dst, value := range linkMatrix[src] {
 			v[dst] = value / sum
 		}
 		stochasticMatrix[src] = v
@@ -37,16 +70,28 @@ func GetStochastixMatrix(linkMatrix Matrix) Matrix {
 /*
 TransitionScore TransitionScore
 */
-func TransitionScore(currentScoreVector Vector, stochasticMatrix Matrix) Vector {
+func TransitionScore(currentScoreVector Vector, stochasticMatrix Matrix) (Vector, error) {
+	if stochasticMatrix == nil || len(stochasticMatrix) == 0 {
+		return currentScoreVector, errors.New("Invalid stochastic matrix")
+	}
+	if currentScoreVector == nil {
+		currentScoreVector = Vector{}
+	}
+	if len(currentScoreVector) == 0 {
+		s := float64(1) / float64(len(stochasticMatrix))
+		for src := range stochasticMatrix {
+			currentScoreVector[src] = s
+		}
+	}
 	score := Vector{}
 
-	for src, vector := range stochasticMatrix {
-		for dst, value := range vector {
+	for src := range stochasticMatrix {
+		for dst := range stochasticMatrix[src] {
 			dstCurrentScore := score[dst]
 			srcCurrentScore := currentScoreVector[src]
-			score[dst] = dstCurrentScore + srcCurrentScore*value
+			score[dst] = dstCurrentScore + srcCurrentScore*stochasticMatrix[src][dst]
 		}
 	}
 
-	return score
+	return score, nil
 }
